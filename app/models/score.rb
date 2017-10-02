865845c2d5e0bb02e2bf9cb837d707d1b55fcc00
Amar A <amaratasia@gmail.com>
2017-10-03 01:55:17 +0530

    Semester Filter

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,4 +2,7 @@ class Score < ApplicationRecord
   belongs_to :exam
   belongs_to :subject
   belongs_to :user
+
+  scope :semester_ilike,  ->(semester)    { where("#{self.to_s.downcase.pluralize}.semester = ?", semester)  if semester.present? }
+
 end
